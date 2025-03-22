a651421ce0e93a55e3c96c3868a728d0196bf396
sai guggilla <guggillaprakash161@gmail.com>
2025-03-22 15:22:20 +0530

    hello

---
+++ L1/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 10;
+var n2 = "2";
+var ans = add(n1, n2);
+console.log(ans);
---
+++ L1/app.ts
@@ -0,0 +1,10 @@
+ function add(num1:number,num2:number)
+ {
+    return num1+num2;
+ }
+
+ const n1=10;
+ const n2=20;
+ const ans=add(n1,n2);
+ console.log(ans);
+ 
